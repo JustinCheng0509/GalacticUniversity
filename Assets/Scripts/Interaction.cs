87b4c3fac13994106b9bd69675fd60647ee43c40
bakedchim <143598844+bakedchim@users.noreply.github.com>
2024-12-05 12:51:10 -0800

    WIP: add overworld scene

---
+++ Assets/Scripts/Interaction.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Interaction
+{
+    // Static readonly variables
+    public static readonly string INTERACTABLE_LAYER = "INTERACTABLE_LAYER";
+    public static readonly string INTERACTABLE_TAG_CLASS = "INTERACTABLE_TAG_CLASS";
+}
---
+++ Assets/Scripts/OverworldInteract.cs
@@ -0,0 +1,57 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class OverworldInteract : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject interactPanel;
+    
+    public InputActionReference interact;
+
+    private string interactableTag = "";
+
+    void Update() {
+        if (interact.action.triggered && interactableTag != "") {
+            StartInteraction(interactableTag);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        // Check if the layer of the object is Interactable
+        if (other.gameObject.layer == LayerMask.NameToLayer(Interaction.INTERACTABLE_LAYER)) {  
+            interactPanel.SetActive(true);
+            interactableTag = other.gameObject.tag;
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer(Interaction.INTERACTABLE_LAYER)) {
+            interactPanel.SetActive(true);
+            interactableTag = other.gameObject.tag;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer(Interaction.INTERACTABLE_LAYER)) {
+            interactPanel.SetActive(false);
+            interactableTag = "";
+        }
+    }
+
+    private void StartInteraction(string tag) {
+        switch (tag) {
+            case var value when value == Interaction.INTERACTABLE_TAG_CLASS:
+                Debug.Log("Interacting with class");
+                break;
+            default:
+                Debug.Log("No interaction found");
+                break;
+        }
+    }
+
+    private void StartClass()
+    {
+        
+    }
+}
